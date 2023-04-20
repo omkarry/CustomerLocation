9984f960dede50c99e0625ba9ae0bc1b637919b3
omkarry <yadavomkar505.com>
2023-04-21 00:20:25 +0530

    Created header and sidebar

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import './App.css';
+import Header from "./Layouts/Header/Header";
+import Main from "./Layouts/Main/Main";
+import axios from "axios";
+
+export const AppContext = React.createContext<any>({});
+
+export const baseUrl = "http://localhost:5026/api";
+
+export type customer = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  customerAddress: {
+    id: number;
+    city: string;
+    state: string;
+    zipCode: number;
+  }
+};
+
+function App() {
+  const [profile, setProfile] = useState({
+    name: "Johnson",
+  });
+  const [loading, setLoading] = useState(true);
+
+  
+
+  useEffect(() => {
+    axios.interceptors.request.use(
+      (config) => {
+        setLoading(true);
+        return config;
+      },
+      (error) => {
+        setLoading(false);
+        return Promise.reject(error);
+      }
+    );
+
+    axios.interceptors.response.use(
+      (response) => {
+        setLoading(false);
+        return response;
+      },
+      (error) => {
+        setLoading(false);
+        return Promise.reject(error);
+      }
+    );
+  }, []);
+
+  
+  return (
+    <AppContext.Provider value={{ profile, loading }}>
+      <BrowserRouter>
+        <div className="container-fluid w-100">
+          <div className="row">
+            <Header />
+          </div>
+          <div className="row">
+
+            <div className="container">
+              <Main />
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    </AppContext.Provider>
+
+  );
+}
+
+export default App;
